a084a17902f7e38ca6b8de7eeb9048913aab2982
Amir Hussein <amir.hussein@student.uts.edu.au>
2020-09-22 21:55:58 +1000

    feat: setup database config and pg pool object

---
+++ src/dbqurries/pool.ts
@@ -0,0 +1,10 @@
+import { Pool } from 'pg';
+import { DB_CONFIG } from '../config';
+
+export const pool = new Pool({
+  user: DB_CONFIG.USERNAME,
+  host: DB_CONFIG.HOST,
+  database: DB_CONFIG.NAME,
+  password: DB_CONFIG.PASSWORD,
+  port: DB_CONFIG.PORT,
+});
