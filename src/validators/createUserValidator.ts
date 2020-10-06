647c22c28db9a0b09afa7c07e4db42d31963366d
Amir Hussein <amir.hussein@student.uts.edu.au>
2020-10-06 14:41:39 +1100

    fix: email validation

--- src/validators/createUserValidator.ts
+++ src/validators/createUserValidator.ts
@@ -2,6 +2,6 @@ import Joi from 'joi';
 
 export const createUserValidator = Joi.object({
   name: Joi.string().min(1).max(50).required(),
-  email: Joi.string().min(1).max(50).required(),
+  email: Joi.string().email().min(1).max(50).required(),
   password: Joi.string().min(8).required(),
 });
